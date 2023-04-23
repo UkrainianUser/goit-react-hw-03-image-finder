4e5b72b8a399048faace006c32a114d934c2b37c
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-04-24 00:06:09 +0300

    create Searchbar

---
+++ src/components/searchbar/Searchbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Searchbar = () => {
+	render() 
+	{
+		return (
+			<header class="searchbar">
+				<form class="form">
+					<button type="submit" class="button">
+						<span class="button-label">Search</span>
+					</button>
+	
+					<input
+						class="input"
+						type="text"
+						autocomplete="off"
+						autofocus
+						placeholder="Search images and photos"
+					/>
+				</form>
+			</header>
+		)
+	};
+
+}
